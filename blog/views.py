c68608b517c4637d8923fe8c3e292b482d8edaa7
Alexandre-Gama <gama.pereira2004@gmail.com>
2025-02-22 18:04:05 -0300

    Importando dados de data.py para a view e depois para o template

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from blog.data import posts
 
 # Create your views here.
 def index(request):
     print('Página Blog acessada')
+
+    context={
+            'text': 'Estamos no Blog',
+            'title': 'Página do Blog - ',
+            'posts': posts
+        }
+    
     return render(
         request,
         'blog/index.html',
-        context={
-            'text': 'Estamos no Blog',
-            'title': 'Página do Blog - '
-        }
+        context
     )
 
 def postagens(request):
     print('Página de postagens do Blog acessada')
-    return render(
-        request,
-        'blog/postagens.html',
-        context={
+
+    context={
             'text': 'Estamos em postagens',
             'title': 'Página de Postagens - '
         }
+    
+    return render(
+        request,
+        'blog/postagens.html',
+        context
     )
--- home/views.py
+++ home/views.py
@@ -3,10 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     print('Home acessada')
+
+    context={
+            'text': 'Estamos na home'
+        }
+    
     return render(
         request,
         'home/index.html',
-        context={
-            'text': 'Estamos na home'
-        }
+        context
     )
