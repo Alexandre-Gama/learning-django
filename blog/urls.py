9df0d323854e3fbecdd4c1bbbf8259a4be98d416
Alexandre-Gama <gama.pereira2004@gmail.com>
2025-02-22 16:58:41 -0300

    Alteração para utilização de css global

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
-    path('', views.index),
-    path('postagens', views.postagens)
+    path('', views.index, name='home'),
+    path('postagens', views.postagens, name='postagens')
 ]
